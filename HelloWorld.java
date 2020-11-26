29dca33552d5d69fbea8ae1649f73bd62f16d92e
DCoxshall <63456774+DCoxshall@users.noreply.github.com>
2020-11-26 21:14:19 +0000

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String args[]) {
+        String greeting = "Hello World";
+		/* The code here took way too long to figure out how to recreate. Fuck Java. */
+		System.out.println(greeting);
+	}
+}
