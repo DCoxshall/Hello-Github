cf64311a67f2782935d0c5834924010a436d9714
DCoxshall <63456774+DCoxshall@users.noreply.github.com>
2021-06-06 20:39:24 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
 	public static void main(String args[]) {
         String greeting = "Hello World";
-		/* The code here took way too long to figure out how to recreate. Fuck Java. */
 		System.out.println(greeting);
 	}
 }
